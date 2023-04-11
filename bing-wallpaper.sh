0aff33f22541a35f53004d503efec283d2cd417d
Ernest Dong <dongcy2000@gmail.com>
2023-04-11 22:29:27 +0800

    use bing.com

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,7 +1,8 @@
 export PATH=/usr/local/bin:$PATH
-URL=$(curl "https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=zh-CN" | jq -r '.url')
+# URL=$(curl "https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=zh-CN" | jq -r '.url')
+URL=$(curl "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN" | jq -r '.images[].url')
 echo $URL
 date=$(date +%Y-%m-%d)
 FILEPATH=~/Pictures/bing-wallpaper/$date.jpg
-curl $URL -o $FILEPATH
+curl http://bing.com$URL -o $FILEPATH
 osascript -e 'tell application "System Events" to tell every desktop to set picture to "'$FILEPATH'"'
