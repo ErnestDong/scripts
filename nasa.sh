23d9b83c1743306e69868352c8afb205bc995128
Ernest Dong <dongcy2000@gmail.com>
2024-04-09 23:12:51 +0800

    nasa

---
+++ nasa.sh
@@ -0,0 +1,10 @@
+export PATH=/usr/local/bin:$PATH
+# URL=$(curl "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" | jq -r '.hdurl')
+# echo $URL
+year=$(gdate --date yesterday +%Y)
+date=$(gdate --date yesterday +%y%m%d)
+FILEPATH=~/Pictures/bing-wallpaper/$date.jpg
+URL="https://apod.cidehom.com/pix/$year/$date.jpg"
+curl $URL -o $FILEPATH
+osascript -e 'tell application "System Events" to tell every desktop to set picture to "'$FILEPATH'"'
+
